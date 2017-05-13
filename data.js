9c5a89bcb0abe15794af86a0a04b0103519c1ac3
Alfred Ball <alfredo.ball@gmail.com>
2017-05-13 17:23:57 -0500

    Initial Commit - Stable

---
+++ data.js
@@ -0,0 +1,79 @@
+﻿var data = [
+    {
+        id: 1,
+        name: 'Pregel',
+        address: '4450 Fortune Ave NW, Concord, NC 28027',
+        lat: 35.426762,
+        long: -80.656011,
+        placeID: 'ChIJKUFjsUQo9ocRedz1bOJgcvo',
+        description: "At the PreGel Training center you can an expect in-depth instruction and guidance. The training centers offer a wide array of classes spanning from beginner and advanced gelato classes, soft serve classes, frozen novelty classes, and other various pastry classes. You will also find their 5-Star Pastry SeriesÂ® which was established for advanced professionals to sharpen and increase their skills in the arts of pastry, plated desserts, chocolate, and showpieces.",
+        featureImg: '/images/PREGEL-TRAINING-CENTER.jpg',
+        url: "https://pregel-itc.com/training/schedule-registration/"
+    },
+    {
+        id: 2,
+        name: 'The Chocolate Academy',
+        address: '600 W Chicago Ave Chicago, IL 60654',
+        lat: 41.897543,
+        long: -87.643774,
+        placeID: 'ChIJwf71t6sts1IRySUYYSsJuX4',
+        description: 'The Chocolate Academy offers practical and theoretical courses for chocolate artisans, pastry chefs, confectioners, bakers and caterers. The curriculum includes beginner, advanced and specialized courses on chocolate processing and a range of techniques and applications - from molding, enrobing and sculpting to decorations and flavorings.',
+        featureImg: '/images/THE-CHOCOLATE-ACADEMY.jpg',
+        url: "http://www.chocolate-academy.com/us/en/calendar"
+    },
+    {
+        id: 3,
+        name: "L'École Valrhona",
+        address: '222 Water St, Brooklyn, NY 11201',
+        lat: 40.702752,
+        long: -73.984889,
+        placeID: 'ChIJGSYRUV4n9ocRWzxy5TgdxN4',
+        description: "L'École Valrhona is for those professionals who want to enhance their technique and expand their knowledge in chocolate and pastry. Class themes will vary throughout the year, from Bonbons, Ice Cream and Mignardises, to Plated Desserts, Introduction to Chocolate, Entremets and Petit Gâteau, Viennoiserie and more",
+        featureImg: "/images/L'ECOLE-VALRHONA.jpg",
+        url: "http://www.valrhonaprofessionals.com/brooklynclasses.html"
+    },
+    {
+        id: 4,
+        name: 'Institute of Culinary Education (ICE)',
+        address: '225 Liberty St, New York, NY 10281',
+        lat: 40.712024,
+        long: -74.015289,
+        placeID: 'ChIJG3SMyrYq9ocRMLdnlmg4cO0',
+        description: " ICE's School of Professional Development you will find intensive programs and training led by industry experts in fields such as bread baking, chocolate making, cake decorating, wine classes, mixology, restaurant management and food media.",
+        featureImg: "/images/THE-INSTITUTE-OF-CULINARY-EDUCATION.jpg",
+        url: "http://recreational.ice.edu/Home/CareerDevelopment"
+    },
+    {
+        id: 5,
+        name: 'Atelier Melissa Coppel',
+        address: '9001 W Sahara Ave, Las Vegas, NV 89117',
+        lat: 36.143133,
+        long: -115.293149,
+        placeID: 'ChIJHx9KaIIq9ocRLutszDWntb8',
+        description: "Atelier Melissa Coppel is a space that represents who she is and the values she believes in. Chef Melissa hosts hands on workshops throughout the year featuring talented chefs from across the globe instructing on subjects such as bon bons, pastries, petit gateaux, travel cakes, tarts, plated desserts, viennoiserie, and more.",
+        featureImg: "/images/ATELIER-MELISSA-COPPEL.jpg",
+        url: "https://www.facebook.com/Atelier-Melissa-Coppel-1525326427687017/"
+    },
+    {
+        id: 6,
+        name: "The French Pastry School",
+        address: '226 W Jackson Blvd #103, Chicago, IL 60606',
+        lat: 41.878229,
+        long: -87.634963,
+        placeID: 'ChIJHx9KaIIq9ocRLutszDWntb8',
+        description: "At The French Pastry School, chefs Jacquy Pfeiffer and SÃ©bastien Canonne, M.O.F., host hands-on master classes to professionals in specialty disciplines of pastry taught by distinguished visiting chefs.",
+        featureImg: "/images/THE-FRENCH-PASTRY-SCHOOL.jpg",
+        url: "www.frenchpastryschool.com/classes/professional"
+    },
+    {
+        id: 7,
+        name: "Jean-Marie Auboine",
+        address: '4780 W Harmon Ave #1, Las Vegas, NV 89103',
+        lat: 36.108375,
+        long: -115.206165,
+        placeID: 'ChIJHx9KaIIq9ocRLutszDWntb8',
+        description: "At the school created by Jean-Marie Auboine, you will find unique masterclass workshops on topics such as panning, bean to bar, sugar showpieces, pannetone, plated desserts and more taught by innovative pastry chefs.",
+        featureImg: "/images/JEAN-MARIE-AUBOINE.jpg",
+        url: "https://www.jmauboinechocolates.com/index.php?route=information/information&information_id=10"
+    }
+];
---
+++ index.js
@@ -0,0 +1,156 @@
+﻿var map = document.getElementById('map');
+
+gmap = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 44.969760, lng: -93.192814 },
+    zoom: 10,
+});
+
+map.Map = gmap;
+
+var input = document.getElementById('pac-input');
+var searchBox = new google.maps.places.SearchBox(input);
+var map = document.getElementById('map').Map;
+var infowindow = new google.maps.InfoWindow();
+var markers = [];
+var selectedMarker = null;
+var filteredLocations = [];
+
+// Bias the SearchBox results towards current map's viewport.
+google.maps.event.addListener(gmap, 'bounds_changed', function () {
+    searchBox.setBounds(gmap.getBounds());
+
+    markers.forEach(function (marker) {
+        marker.setMap(null);
+    });
+
+    markers = [];
+
+    data.forEach(function (dataItem) {
+        if (gmap.getBounds().contains(new google.maps.LatLng(dataItem.lat, dataItem.long))) {
+            var marker = new google.maps.Marker({
+                map: gmap,
+                position: new google.maps.LatLng(dataItem.lat, dataItem.long),
+                icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+            });
+
+            markers.push(marker);
+        }
+    });
+
+    setFilteredLocations();
+    refreshLocations();
+});
+
+// Listen for the event fired when the user selects a prediction and retrieve
+// more details for that place.
+searchBox.addListener('places_changed', function () {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+        return;
+    }
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+        if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
+    });
+    gmap.fitBounds(bounds);
+
+    markers.forEach(function (marker) {
+        marker.setMap(null);
+    });
+
+    markers = [];
+
+    data.forEach(function (dataItem) {
+        if (bounds.contains(new google.maps.LatLng(dataItem.lat, dataItem.long))) {
+            var marker = new google.maps.Marker({
+                map: gmap,
+                position: new google.maps.LatLng(dataItem.lat, dataItem.long),
+                icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+            });
+
+            markers.push(marker);
+        }
+    });
+});
+
+function setFilteredLocations()
+{
+    filteredLocations = [];
+    var bounds = gmap.getBounds();
+    data.forEach(function (dataItem) {
+        if (bounds.contains(new google.maps.LatLng(dataItem.lat, dataItem.long))) {
+            filteredLocations.push(dataItem);
+        }
+    });
+}
+
+function refreshLocations()
+{
+    document.getElementById('locations').innerHTML = "";
+    console.log("Refreshed")
+    for (var i = 0; i < 3 - 1; i++) {
+        if (filteredLocations.length >= i + 1) {
+            var dataItem = filteredLocations[i];
+
+            var content = document.createElement("div");
+
+            var school = document.createElement("div");
+
+            var link = document.createElement("a");
+
+            var schoolImage = document.createElement("img");
+
+            var name = document.createElement("div");
+
+            $(name).text(dataItem.name);
+
+            $(link).attr('href', dataItem.url);
+            $(schoolImage).attr('src', dataItem.featureImg);
+
+            $(school).append(name);
+            $(school).append(schoolImage);
+
+            $(link).append(school);
+
+            $(content).append(link);
+
+            $(content).addClass('location-content');
+
+            $(school).addClass('location-content-school');
+
+            $('#locations').append(content);
+        }
+    }
+}
+
+function onchange(e) {
+
+    infowindow.close();
+
+    var selectedOption = list.selectedOptions[0];
+
+    var id = $(selectedOption).val();
+
+    var dataItem = data.filter(function (item) {
+        return item.id == id;
+    })[0];
+
+    $('#featureImg').attr('src', dataItem.featureImg);
+    $('#description').text(dataItem.description);
+
+    map.setZoom(10);
+    map.setCenter(new google.maps.LatLng(dataItem.lat, dataItem.long));
+}
